e344d31dc3443c677528cf69765f8507562d904e
baonq <nguyenducbaocv@gmail.com>
2025-05-25 11:33:21 +0700

    init

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: export PYTHONPATH=./ && adk web --port $PORT --host 0.0.0.0
+web: export PYTHONPATH=./ && adk web --port 8080 --host 0.0.0.0
+api: export PYTHONPATH=./ && python server/main.py
--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root() -> dict[str, str]:
     return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
